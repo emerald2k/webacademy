682bed1e41f1ed45cb4d106d04b6f6e26232be5f
Bogdan Matei <mateipetrutbogdan@gmail.com>
2026-03-26 22:00:49 +0200

    feat: add lectia 7

---
+++ cursuri/lectia7/script.js
@@ -0,0 +1,93 @@
+/* function func() {
+  alert('!');
+} */
+
+/* function func1() {
+  alert('1');
+}
+function func2() {
+  alert('2');
+} */
+
+/* function func() {
+  var elem = document.getElementById('test');
+  alert(elem.value);
+} */
+
+/* function func() {
+  var elem = document.getElementById('test');
+  alert(elem.value);
+  alert(elem.id);
+  alert(elem.type);
+} */
+
+/* function func() {
+  var elem = document.getElementById('test');
+  elem.value = 'www';
+}
+ */
+
+/* function func() {
+  alert(document.getElementById('test').value);
+} */
+
+/* function func() {
+  document.getElementById('test').value = 'www';
+} */
+
+/* function func() {
+  var elem = document.getElementById('test');
+  elem.value = 'www';
+  elem.type = 'submit';
+}
+ */
+
+/* function func() {
+  document.getElementById('test').value = 'www';
+  document.getElementById('submit').type = 'submit';
+} */
+
+/* function func() {
+  var elem = document.getElementById('test');
+  alert(elem.ClassName);
+} */
+
+/* function func1() {
+  var elem = document.getElementById('test1');
+  alert(elem.value);
+}
+function func2() {
+  var elem = document.getElementById('test2');
+  alert(elem.value);
+}
+function func3() {
+  var elem = document.getElementById('test3');
+  alert(elem.value);
+} */
+
+/* function func(id) {
+  var elem = document.getElementById(id);
+  alert(elem.value);
+} */
+
+/* function func(elem) {
+  alert(elem.value);
+}
+ */
+
+/* function func() {
+  var elem = document.getElementById('test');
+  elem.style.color = 'red';
+} */
+
+// event bubbling handle
+
+/* for (var i = 1; i <= 100; i++) {
+  document.writeln(i + '<br>');
+} */
+
+var arr = [];
+for (var i = 0; i < 10; i++) {
+  arr.push(Math.random().toFixed(2));
+}
+console.log(arr);
